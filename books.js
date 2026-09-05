22d5a9c8350df2d43ca66767b74a3c04eb9f72cb
accel <accel@MacBook-Air.local>
2026-09-06 01:19:33 +0900

    pdfを差し替え

--- books.js
+++ books.js
@@ -2,16 +2,16 @@ window.BOOKS = [
   {
     id: 'sample',
     file: 'sample.pdf',
-    name: 'サンプル冊子'
+    name: 'どっち？'
   },
   {
     id: 'sample2',
     file: 'sample2.pdf',
-    name: 'サンプル冊子 2'
+    name: 'TKG'
   },
   {
     id: 'sample3',
     file: 'sample3.pdf',
-    name: 'サンプル冊子 3'
+    name: '金の斧銀の斧'
   }
 ];
