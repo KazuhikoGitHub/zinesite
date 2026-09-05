385089f900a5f358920ade540b21f6b58d83ec70
accel <accel@MacBook-Air.local>
2026-09-05 23:20:26 +0900

    ハンバーガーメニュー追加

---
+++ books.js
@@ -0,0 +1,17 @@
+window.BOOKS = [
+  {
+    id: 'sample',
+    file: 'sample.pdf',
+    name: 'サンプル冊子'
+  },
+  {
+    id: 'sample2',
+    file: 'sample2.pdf',
+    name: 'サンプル冊子 2'
+  },
+  {
+    id: 'sample3',
+    file: 'sample3.pdf',
+    name: 'サンプル冊子 3'
+  }
+];
